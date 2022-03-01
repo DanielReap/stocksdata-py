457c530d2b96a045dccedeee1390f54e03fecdce
Dan <danielreapsome@gmail.com>
2022-03-01 15:53:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from polygon import RESTClient
 
 #creating defition main
 def main():
+    #user-inputed ticker
+    ticker = input("What ticker would you like to get information on? ")
+
     #api key
     key = "9r904djsCqkANtfuHsVvqUDppDbSHSZ7"
 
@@ -12,12 +15,20 @@ def main():
     now = datetime.date.today()
 
     #with statement that turns RESTCLIENT(key) to object client
-    with RESTClient(key) as client:
-        #checks information from yesterday
-        resp = client.stocks_equities_daily_open_close("AAPL", (now - datetime.timedelta(days=1)))
-        #prints variables given from the response
-        print(vars(resp))
-        #print(f"On: {resp.from_} Apple opened at {resp.open} and closed at {resp.close}")
+    try:
+        with RESTClient(key) as client:
+            #checks information from yesterday
+            resp = client.stocks_equities_daily_open_close(str(ticker), (now - datetime.timedelta(days=1)))
+            #prints variables given from the response
+            print(vars(resp))
+            #print(f"On: {resp.from_} Apple opened at {resp.open} and closed at {resp.close}")
+    except Exception as e:
+        error = e.__class__.__name__
+        if error == 'HTTPError':
+            print('Invalid ticker, try again.')
+            main()
+        else:
+            print(error)
 
 #runs function
 if __name__ == '__main__':
