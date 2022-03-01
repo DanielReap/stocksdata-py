198cd57af14b73408b026f91f44a40708fa406a5
Dan <danielreapsome@gmail.com>
2022-03-01 14:08:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ def main():
 
     now = datetime.date.today()
 
-    # RESTClient can be used as a context manager to facilitate closing the underlying http session
-    # https://requests.readthedocs.io/en/master/user/advanced/#session-objects
     with RESTClient(key) as client:
         resp = client.stocks_equities_daily_open_close("AAPL", (now - datetime.timedelta(days=1)))
         print(f"On: {resp.from_} Apple opened at {resp.open} and closed at {resp.close}")
